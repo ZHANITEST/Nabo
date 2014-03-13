8e899b7b5aa72e1e32dfbd90604f5a654d1865c1
XKY <despair4045@naver.com>
2014-03-14 02:00:21 +0900

    fix bugs. add some methods

--- nabo.py
+++ nabo.py
@@ -23,6 +23,9 @@ class NaboError(Exception):pass
 # class: nabo
 #============================================================
 class Nabo:
+	#========================================================
+	# __init__
+	#========================================================
 	def __init__(self, username):
 		# 블로그 데이터 정의
 		self.DATA = {
@@ -30,21 +33,16 @@ class Nabo:
 			"BLOG_URL"	:	"http://blog.naver.com/"+ username,
 			"POST_BODY"	: None 
 		}
-
-
-
+	#========================================================
+	# open
+	#========================================================
 	def open(self, url):
 		userid = self.DATA["USER_ID"]
 		
-		# 입력한 url 끝에 슬래쉬 지우기
-		if url[-1] == "/":
-			url = url[:-1]
-		
-		# blog.naver.com 패턴일 때
 		rep = re.match( "http://blog.naver.com/"+userid+"/(\d+)", url )
 		if rep != None:
 			self.DATA["POST_ID"] = rep.group(1)
-		
+			
 		# blog.me 패턴일 때
 		elif rep == None:
 			rep = re.search( "http://"+userid+".blog.me/(\d+)", url )
@@ -53,31 +51,51 @@ class Nabo:
 			else:
 				raise NaboError, "Can't found post id"
 		
+		# 입력한 url 끝에 슬래쉬 지우기
+		if url[-1] == "/":
+			url = url[:-1]
+		
+		# blog.naver.com 패턴일 때
+
+		
 		self.DATA["POST_FRAMEURL"] = "http://blog.naver.com/PostView.nhn?blogId="+ userid +"&logNo="+ self.DATA["POST_ID"] + "&redirect=Dlog&widgetTypeCall=true"
 		
 		# HTML 리퀘스트 요청
-		req = urllib2.Request( self.DATA["POST_FRAMEURL"] )
-		html = urllib2.urlopen( req ).read()
+		hea = { "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0" }
+		req = urllib2.Request( self.DATA["POST_FRAMEURL"], headers=hea )
+		self.HTML = urllib2.urlopen( req ).read()
 		
 		# 패턴 람다식
 		search = ( lambda x,y : re.search(x, y).group(1) )
 
-
-		
 		# 날짜 읽기
 		#\d{4}/\d{2}/\d{2} .+:\d\d
-		self.DATA["POST_DATE"] = search( "(\d{4}/\d{2}/\d{2} .+:\d\d)", html)
+		self.DATA["POST_DATE"] = search( "(\d{4}/\d{2}/\d{2} .+:\d\d)", self.HTML)
 		
 		# 제목 읽기
 		#self.DATA["POST_TITLE"] = re.search( "<title>(.+):", html ).group(1)
-		self.DATA["POST_TITLE"] = search( "<title>(.+):", html )
+		self.DATA["POST_TITLE"] = search( "<title>(.+):", self.HTML )
 		
 		#본문 읽기
 		# r0: post-view[\d]+" .+">[\s]+(<p>.+)</div>
 		# r1: post-view[\d]+\" .+\">[\s]+(.+)</div>
-		self.DATA["POST_BODY"] = search( "post-view[\d]+\" .+\">[\s]+(.+)</div>", html )
+		self.DATA["POST_BODY"] = search( "post-view[\d]+\" .+\">[\s]+(.+)</div>", self.HTML )
 		
-	
-	def getIMGs(self):
+	#========================================================
+	# getIMGs
+	#========================================================
+	def getIMGs( self ):
 		lobj = re.search( "(http://[\w\s\d./_-]+.type=w2)", self.DATA["POST_BODY"] ).groups()
 		return lobj
+	#========================================================
+	# getHTML
+	#========================================================
+	def getHTML( self ):
+		return self.HTML
+	#========================================================
+	# fopen
+	#========================================================
+	def fOPEN( self, fname ):
+		f = open( fname, "w" )
+		f.write( self.HTML )
+		f.close()
