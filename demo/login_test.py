b3c1bcf7c0286483d73169703a0fe5b5b174740d
XKY <mozirikan@gmail.com>
2014-05-06 19:15:41 +0900

    add login demo.

---
+++ demo/login_test.py
@@ -0,0 +1,16 @@
+#===================================================================================================
+#
+# login_test.py (for Nabo)
+# 2014, XKY
+# License: LGPL v3
+#
+#===================================================================================================
+from nabo import Nabo #Parser
+from nabo import Conv #HTML code processor
+
+if __name__ == "__main__":
+	# Target: http://blogpeople.blog.me/150181071013
+	id = raw_input( "Your id: ")
+	pw = raw_input( "Your pw: ")
+	test = Nabo( id )
+	test.login( pw )
